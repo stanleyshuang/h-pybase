87fe8d2e6eb710765471b91a97b0b0b15ebfc62f
stanleyshuang <stanleyshuang@qnap.com>
2021-02-15 01:19:01 +0800

    overall ruleset analysis reports

--- src/server/area51/views.py
+++ src/server/area51/views.py
@@ -36,16 +36,21 @@ def check_key_unique_value(objs, key, b_detail=False, ignored_strs = [], counted
             extracted = [i for i in extracted if subs not in i]
 
     # unique strings
-    extracted_set = set(extracted)
-    extracted_num = len(extracted_set)
+    extracted_dict = {}
+    for item in extracted:
+        if item in extracted_dict:
+            extracted_dict[item] += 1
+        else:
+            extracted_dict[item] = 1
+    extracted_num = len(extracted_dict)
 
     output = '{key} number={extracted_num}<br>'.format(key=key, extracted_num=extracted_num)
     if b_detail:
-        for i, (key, value) in enumerate(keycounts.items()):
-            output += '&nbsp;&nbsp;&nbsp;&nbsp;' + key + ' : ' + str(value) + ' : ' + format(value/objnum*100.0, '.2f') + '%<br>'
+        for key in sorted(keycounts.keys()):
+            output += '&nbsp;&nbsp;&nbsp;&nbsp;' + str(key) + ' | ' + str(keycounts[key]) + ' | ' + format(keycounts[key]/objnum*100.0, '.1f') + '%<br>'
         output += '<br>'
-        for item in extracted_set:
-            output += '&nbsp;&nbsp;&nbsp;&nbsp;' + str(item) + '<br>'
+        for key in sorted(extracted_dict.keys()):
+            output += '&nbsp;&nbsp;&nbsp;&nbsp;' + str(key) + ' | ' + str(extracted_dict[key]) + ' | ' + format(extracted_dict[key]/objnum*100.0, '.1f') + '%<br>'
     output += '<br>'
     return output
 
@@ -62,11 +67,13 @@ def dump_objs(objs):
                                                              ['created_at', 'updated_at', 'former_category', 'deployment', 'signature_severity', 'attack_target', 'affected_product', 'malware_family', 'performance_impact', 'tag', 'cve'])
     output += check_key_unique_value(objs, 'options', True, ['sid:', 'metadata:', 'msg:', 'content:', 'reference:', 'classtype:', 'rev:', 'id:', 'nocase;', 'flow:', 'distance:', 'depth:', 'pcre:', 'within:', 'flowbits:', 'threshold:', 'byte_test:', 'offset:', 'bsize:', 'isdataat:', 'dsize:', 'urilen:', 'fast_pattern:', 'byte_extract:', 'stream_size:', 'asn1:', 'base64_data;', 'base64_decode:', 'byte_jump:', 'detection_filter:', 'dns.query;', 'dns_query;', 'dotprefix;', 'endswith;', 'fast_pattern;', 'file.data;', 'file_data;', 'flags:', 'ftpbounce;', 'icode:', 'itype:', 'ip_proto:', 'noalert;', 'ja3.hash;', 'ja3.string;', 'ja3_hash;', 'ja3s.hash;', 'http.accept;', 'http.accept_enc;', 'http.accept_lang;', 'http.connection;', 'http.content_len;', 'http.content_type;', 'http.cookie;', 'http.header.raw;', 'http.header_names;', 'http.header;', 'http.host.raw;', 'http.host;', 'http.location;', 'http.method;', 'http.protocol;', 'http.server;', 'http.start;', 'http.uri.raw;', 'http.referer;', 'http.request_body;', 'http.request_line;', 'http.response_body;', 'http.response_line;', 'http.stat_code;', 'http.stat_msg;', 'http.uri;', 'http.user_agent;', 'http_header_names;', 'http_uri;', 'http_user_agent;', 'rawbytes;', 'ssh_proto;', 'ssl_state:', 'ssl_version:', 'startswith;', 'tag:', 'tls.cert_issuer;', 'tls.cert_serial;', 'tls.cert_subject;', 'tls.sni;', 'ttl:', 'xbits:'],
                                                             ['sid:', 'metadata:', 'msg:', 'content:', 'reference:', 'classtype:', 'rev:', 'id:', 'nocase;', 'flow:', 'distance:', 'depth:', 'pcre:', 'within:', 'flowbits:', 'threshold:', 'byte_test:', 'offset:', 'bsize:', 'isdataat:', 'dsize:', 'urilen:', 'fast_pattern:', 'byte_extract:', 'stream_size:', 'asn1:', 'base64_data;', 'base64_decode:', 'byte_jump:', 'detection_filter:', 'dns.query;', 'dns_query;', 'dotprefix;', 'endswith;', 'fast_pattern;', 'file.data;', 'file_data;', 'flags:', 'ftpbounce;', 'icode:', 'itype:', 'ip_proto:', 'noalert;', 'ja3.hash;', 'ja3.string;', 'ja3_hash;', 'ja3s.hash;', 'http.accept;', 'http.accept_enc;', 'http.accept_lang;', 'http.connection;', 'http.content_len;', 'http.content_type;', 'http.cookie;', 'http.header.raw;', 'http.header_names;', 'http.header;', 'http.host.raw;', 'http.host;', 'http.location;', 'http.method;', 'http.protocol;', 'http.server;', 'http.start;', 'http.uri.raw;', 'http.referer;', 'http.request_body;', 'http.request_line;', 'http.response_body;', 'http.response_line;', 'http.stat_code;', 'http.stat_msg;', 'http.uri;', 'http.user_agent;', 'http_header_names;', 'http_uri;', 'http_user_agent;', 'rawbytes;', 'ssh_proto;', 'ssl_state:', 'ssl_version:', 'startswith;', 'tag:', 'tls.cert_issuer;', 'tls.cert_serial;', 'tls.cert_subject;', 'tls.sni;', 'ttl:', 'xbits:'])
+    '''
     for obj in objs:
         # output += json.dumps(obj) + '<br>'
         output += 'header: ' + obj['header'] + '<br>'
         output += 'metadata: ' + dump_list(obj['metadata']) + '<br>'
         output += 'options: ' + dump_list(obj['options']) + '<br>'
+    '''
     return output
 
 def dump_list(the_list):
