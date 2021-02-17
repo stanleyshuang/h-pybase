914986cfcc163f8028d5d2b3ad33d267d85c5057
stanleyshuang <stanleyshuang@qnap.com>
2021-02-17 23:32:50 +0800

    add vue client v2

--- _build/mac-lab/run.sh
+++ _build/mac-lab/run.sh
@@ -15,25 +15,22 @@ if ! [ -d "$apphome/venv" ]; then
           pip install -r $env/config/requirements.txt
   fi
 
-  # Veu environment
-  work_path=$(dirname $0)
-  cd $work_path
-  base_dir=$(pwd)
-
-  echo "cd $apphome"
-        cd $apphome
-  echo "vue create client"
-        vue create client
-
-  # pop up
   echo "deactivate"
         deactivate
-  cd $base_dir
 fi
 
 ### 2. Run app script
 echo "-- Run the following script ----"
 echo "cd $apphome/"
 echo "source $apphome/venv/bin/activate"
+# Veu environment
+if ! [ -d "$apphome/client" ]; then
+  echo "cd $apphome"
+  echo "vue create client"
+else
+  echo "cd $apphome/client"
+  echo "npm install"
+fi
+echo "cd $apphome"
 echo "npm run build"
 echo "npm start"
