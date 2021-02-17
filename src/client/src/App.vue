fb3288d91ce38995529f4f895310c57a05139742
stanleyshuang <stanleyshuang@qnap.com>
2021-02-17 23:21:49 +0800

    add vue client

---
+++ src/client/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Ping msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import Ping from './components/Ping.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Ping
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
