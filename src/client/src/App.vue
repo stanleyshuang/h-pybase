acbc06fa80d8d269344ad0e1166c5ed1f939d8e1
stanleyshuang <stanleyshuang@qnap.com>
2021-02-20 23:46:20 +0800

    heroku enabled v2

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Ping msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Ping from './components/Ping.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Ping
   }
 }
 </script>
